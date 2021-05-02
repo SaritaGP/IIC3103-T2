85b3ed33b8aa225d841eb0d4a54d50779789be59
SaritaGP <saritabelen.gp@gmail.com>
2021-05-02 10:13:40 -0400

    puts y error 400

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::BaseController < Api::BaseController
+  rescue_from("ActionController::ParameterMissing") do
+    render json: nil, status: 400
+  end
   before_action do
     self.namespace_for_serializer = ::Api::V1
   end
